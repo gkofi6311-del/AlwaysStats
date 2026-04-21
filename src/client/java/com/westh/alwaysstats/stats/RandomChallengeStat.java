ebe32cec2e7f80396dea86eeb7a74e22a371bf05
gkofi6311-del <gkofi6311@gmail.com>
2026-04-21 16:37:48 +0200

    Refactor RandomChallengeStat to include KeyBinding

--- src/client/java/com/westh/alwaysstats/stats/RandomChallengeStat.java
+++ src/client/java/com/westh/alwaysstats/stats/RandomChallengeStat.java
@@ -1,6 +1,34 @@
-public static final KeyBinding CHALLENGE_KEY = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-    "key.alwaysstats.random_challenge",  // translation key for the keybind
-    InputUtil.Type.KEYSYM,
-    GLFW.GLFW_KEY_UNKNOWN,
-    "key.categories.alwaysstats"         // translation key for category - this is fine as String
-));
+package com.westh.alwaysstats.stats;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class RandomChallengeStat implements StatProvider {
+    
+    public static final KeyBinding CHALLENGE_KEY = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.alwaysstats.random_challenge",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_UNKNOWN,
+            "key.categories.alwaysstats" // This String is correct HERE
+    ));
+
+    @Override
+    public String getLabel() {
+        return "Challenge";
+    }
+
+    @Override
+    public String getValue() {
+        return "Press " + CHALLENGE_KEY.getBoundKeyLocalizedText().getString();
+    }
+
+    @Override
+    public int getColor() {
+        return 0xFF5555; // red
+    }
+    
+    // If you had a method that took Category instead of String, delete that line.
+    // The "key.categories.alwaysstats" string only belongs in the KeyBinding constructor above.
+}
