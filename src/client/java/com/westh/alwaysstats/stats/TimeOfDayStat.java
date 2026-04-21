2c9e8dfa16849fb1a4de5b17d94284eb59150013
gkofi6311-del <gkofi6311@gmail.com>
2026-04-21 16:16:11 +0200

    Rename TimeOfDayStat to MinecraftDayStat

--- src/client/java/com/westh/alwaysstats/stats/TimeOfDayStat.java
+++ src/client/java/com/westh/alwaysstats/stats/TimeOfDayStat.java
@@ -2,30 +2,18 @@ package com.westh.alwaysstats.stats;
 
 import net.minecraft.client.Minecraft;
 
-public class TimeOfDayStat implements StatProvider {
+public class MinecraftDayStat implements StatProvider {
 
     @Override
-    public String getConfigKey() {
-        return "timeOfDay";
-    }
+    public String getConfigKey() { return "mcDay"; }
 
     @Override
-    public String getConfigName() {
-        return "Time";
-    }
+    public String getConfigName() { return "Day"; }
 
     @Override
     public String getDisplayText(Minecraft client) {
-        long timeOfDay = client.level.getDayTime() % 24000;
-
-        // Convert to hours and minutes (0 ticks = 6:00 AM in Minecraft)
-        int hours = (int) ((timeOfDay / 1000 + 6) % 24);
-        int minutes = (int) ((timeOfDay % 1000) * 60 / 1000);
-
-        // Player can sleep between tick 12542 and 23460
-        boolean canSleep = timeOfDay >= 12542 && timeOfDay <= 23460;
-        String sleepIndicator = canSleep ? " *" : "";
-
-        return String.format("%s: %02d:%02d%s", getConfigName(), hours, minutes, sleepIndicator);
+        if (client.level == null) return null;
+        long day = (client.level.getDayTime() / 24000L) + 1;
+        return getConfigName() + ": " + day;
     }
 }
